8345a5f00a905dbcb8d4c9c842b5cdf80f1fc210
SHADIL VV <shadilshad508@gmail.com>
2026-03-12 14:12:48 +0530

    Add FinalKeyword.java to demonstrate final keyword usage

---
+++ FinalKeyword.java
@@ -0,0 +1,8 @@
+class Main {
+    
+    //When you do not want a variable's value to change, use the final keyword.//
+    public static void main(String[] args) {
+        final int Time = 60;
+        System.out.println(Time + " Seconds");
+    }
+}
