048351a04b0178d2afd4c7075615757ac030f414
SHADIL VV <shadilshad508@gmail.com>
2026-03-12 15:55:44 +0530

    Create JavaString.java

---
+++ JavaString.java
@@ -0,0 +1,8 @@
+public class Number{
+    public static void main(String[] args){
+        String word = "SUPERCALIFRAGLISTICEXPLODEOUS";
+        System.out.println(word.indexOf("EXPLODEOUS"));
+        
+        System.out.print(word.charAt(21));
+    }
+}
