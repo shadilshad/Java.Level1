0a6b19e1ba8fa0c9117086522f120404dc8b267b
SHADIL VV <shadilshad508@gmail.com>
2026-03-12 14:46:00 +0530

    Create var.java
    var that can be used for all datatypes

---
+++ var.java
@@ -0,0 +1,13 @@
+class Main {
+    
+    public static void main(String[] args) {
+        var name = " shadil";
+        var chara = ' c';
+        var number = 33;
+        Object[] box = {name, chara, number};
+        for(int i = 0 ; i< box.length; i++){
+            System.out.println("The number "+ i + box[i]);
+        }
+
+    }
+}
