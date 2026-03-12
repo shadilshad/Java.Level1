b60eb128fd9b77d5d45f70ff1d4284fe041b9c6b
SHADIL VV <shadilshad508@gmail.com>
2026-03-12 15:43:42 +0530

    Create increment.java

---
+++ increment.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    x += 5;
+    x -= 5;
+    int y = 10;
+    ++y;
+    y += 8;
+    
+    System.out.println(x);
+    System.out.println(y);
+    
+  }
+}
