ad11c2e79d7c2dac8cf62251941aeddb2281dc94
SHADIL VV <shadilshad508@gmail.com>
2026-06-15 11:43:34 +0530

    Loops

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class Reverse {
+     public static void main(String[] args) {
+        int num = 987654321;
+        int reverse =0;
+
+        while(num != 0){
+            int digit = num % 10;
+            reverse = reverse*10 + digit;
+            num = num/10;
+        }
+        System.out.println(reverse);
+     }   
+}
