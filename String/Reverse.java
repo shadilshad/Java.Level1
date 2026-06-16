fa03627b5b48a3bb8a28c4a7f8fae95ed3a83d18
SHADIL VV <shadilshad508@gmail.com>
2026-06-16 17:07:11 +0530

    java practice

---
+++ String/Reverse.java
@@ -0,0 +1,16 @@
+package String;
+
+public class Reverse {
+    public static void main(String[] args) {
+        
+        String name = "SHADIL";
+        String rev = "";
+
+        for(int i = name.length()-1; i>=0; i -- ){
+            rev = rev + name.charAt(i);
+        }
+
+        System.out.println(rev);
+    }
+    
+}
